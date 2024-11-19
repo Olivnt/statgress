b7affb3cbb8eb6e34470607f1bf2dfdf44be5dbc
Olivnt <olivnt@yahoo.co.uk>
2024-11-19 01:09:02 +0100

    Update Case_Study_-_Customer_Satisfaction_Prediction.py

--- pages/Case_Study_-_Customer_Satisfaction_Prediction.py
+++ pages/Case_Study_-_Customer_Satisfaction_Prediction.py
@@ -16,7 +16,7 @@ page = st.sidebar.radio("Go to", ["Main Page", "View Full Dataset"])
 # Data Loading Function
 @st.cache_data
 def load_data():
-    return pd.read_csv('dataSets\\Invistico_Airline.csv')
+    return pd.read_csv('myPortfolio/dataSets/Invistico_Airline.csv')
 
 data = load_data()
 
